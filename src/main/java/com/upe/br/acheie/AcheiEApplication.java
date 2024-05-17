ab2007764152521bc800c8d66ddb4b6179335db2
MateusVasc <109766889+MateusVasc@users.noreply.github.com>
2024-05-17 15:07:51 -0300

    configuração initial do projeto

---
+++ src/main/java/com/upe/br/acheie/AcheiEApplication.java
@@ -0,0 +1,13 @@
+package com.upe.br.acheie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcheiEApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcheiEApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upe/br/acheie/AcheiEApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upe.br.acheie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcheiEApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
