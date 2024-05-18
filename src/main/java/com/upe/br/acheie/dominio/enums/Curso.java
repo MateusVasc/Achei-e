ee56962b73d56718eb1751b4375431dfb2728128
MateusVasc <109766889+MateusVasc@users.noreply.github.com>
2024-05-17 21:12:50 -0300

    adicionando enum Curso

---
+++ src/main/java/com/upe/br/acheie/dominio/enums/Curso.java
@@ -0,0 +1,20 @@
+package com.upe.br.acheie.dominio.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Curso {
+
+  ENGENHARIA_DE_SOFTWARE("Engenharia de Software"),
+  LICENCIATURA_EM_COMPUTACAO("Licenciatura em Computação"),
+  MEDICINA("Medicina"),
+  PSICOLOGIA("Psicologia"),
+  LICENCIATURA_EM_HISTORIA("Licenciatura em Historia"),
+  LICENCIATURA_EM_GEOGRAFIA("Licenciatura em Geografia"),
+  LICENCIATURA_EM_CIENCIAS_BIOLOGICAS("Licenciatura em Ciencias Biologicas"),
+  LICENCIATURA_EM_LETRAS("Licenciatura em Letras"),
+  LICENCIATURA_EM_PEDAGOGIA("Licenciatura em Pedagogia");
+  private final String valor;
+}
