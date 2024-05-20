82066c333fb28c07ce1a251de331d04bbabbe2d8
MateusVasc <109766889+MateusVasc@users.noreply.github.com>
2024-05-20 14:00:53 -0300

    atualizando entidade Usuario

--- src/main/java/com/upe/br/acheie/dominio/modelos/Usuario.java
+++ src/main/java/com/upe/br/acheie/dominio/modelos/Usuario.java
@@ -1,5 +1,6 @@
 package com.upe.br.acheie.dominio.modelos;
 
+import com.upe.br.acheie.dominio.dto.CadastroRequest;
 import com.upe.br.acheie.dominio.enums.Curso;
 import com.upe.br.acheie.dominio.enums.Periodo;
 import jakarta.persistence.CascadeType;
@@ -15,6 +16,7 @@ import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Email;
+import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 import java.util.Set;
@@ -22,12 +24,15 @@ import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class Usuario {
+public class Usuario implements UserDetails {
 
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
@@ -83,4 +88,50 @@ public class Usuario {
   @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true)
   private List<Mensagem> mensagens;
 
+  public Usuario(CadastroRequest request) {
+    this.nome = request.nome();
+    this.sobrenome = request.sobrenome();
+    this.email = request.email();
+    this.senha = request.senha();
+    this.curso = request.curso();
+    this.periodo = request.periodo();
+    this.telefone = request.telefone();
+    this.foto = request.foto();
+    this.criacaoDaConta = request.criacaoDaConta();
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of(new SimpleGrantedAuthority("USUARIO"));
+  }
+
+  @Override
+  public String getPassword() {
+    return this.senha;
+  }
+
+  @Override
+  public String getUsername() {
+    return this.email;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
 }
