da0bb0144e76b247195218134427ecc070dcd438
MateusVasc <109766889+MateusVasc@users.noreply.github.com>
2024-05-20 14:05:05 -0300

    adicionando Serviço AutenticacaoServico

---
+++ src/main/java/com/upe/br/acheie/servico/AutenticacaoServico.java
@@ -0,0 +1,76 @@
+package com.upe.br.acheie.servico;
+
+import com.upe.br.acheie.config.TokenServico;
+import com.upe.br.acheie.dominio.dto.CadastroRequest;
+import com.upe.br.acheie.dominio.dto.CadastroResponse;
+import com.upe.br.acheie.dominio.dto.LoginRequest;
+import com.upe.br.acheie.dominio.dto.LoginResponse;
+import com.upe.br.acheie.dominio.modelos.Usuario;
+import com.upe.br.acheie.repositorio.UsuarioRepositorio;
+import com.upe.br.acheie.utils.AcheieException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AutenticacaoServico {
+
+  private final AuthenticationManager authenticationManager;
+  private final TokenServico tokenServico;
+  private final UsuarioRepositorio usuarioRepositorio;
+
+  public LoginResponse autenticarLogin(LoginRequest request) throws AcheieException {
+    var usuarioData = new UsernamePasswordAuthenticationToken(request.email(), request.senha());
+    var auth = this.authenticationManager.authenticate(usuarioData);
+
+    var token = tokenServico.gerarToken((Usuario) auth.getPrincipal());
+    return new LoginResponse(token);
+  }
+
+  public CadastroResponse cadastrarUsuario(CadastroRequest request) throws AcheieException {
+    if (!validarInfo(request)) {
+      throw new AcheieException("Informe informações válidas");
+    }
+
+    if (this.usuarioRepositorio.findByEmail(request.email()) != null) {
+      throw new AcheieException("Este e-mail já está cadastrado");
+    }
+
+    Usuario usuarioNovo = new Usuario(request);
+    usuarioNovo.setSenha(new BCryptPasswordEncoder().encode(request.senha()));
+    this.usuarioRepositorio.save(usuarioNovo);
+
+    return new CadastroResponse(request.email(), request.criacaoDaConta());
+  }
+
+  private boolean validarInfo(CadastroRequest request) {
+    return validarNome(request.nome()) &&
+        validarSobrenome(request.sobrenome()) &&
+        validarEmail(request.email()) &&
+        validarSenha(request.senha()) &&
+        validarTelefone(request.telefone());
+  }
+
+  private boolean validarNome(String nome) {
+    return nome != null && !nome.trim().isEmpty() && nome.length() > 4;
+  }
+
+  private boolean validarSobrenome(String sobrenome) {
+    return sobrenome != null && !sobrenome.trim().isEmpty() && sobrenome.length() > 4;
+  }
+
+  private boolean validarEmail(String email) {
+    return email != null && !email.trim().isEmpty() && email.length() > 4;
+  }
+
+  private boolean validarSenha(String senha) {
+    return senha != null && !senha.trim().isEmpty() && senha.length() > 4;
+  }
+
+  private boolean validarTelefone(String telefone) {
+    return telefone != null && !telefone.trim().isEmpty() && telefone.length() > 4;
+  }
+}
