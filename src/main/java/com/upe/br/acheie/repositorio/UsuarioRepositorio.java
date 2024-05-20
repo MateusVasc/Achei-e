e6a9bea05198109453c9bd41c598aa46a036f1f4
MateusVasc <109766889+MateusVasc@users.noreply.github.com>
2024-05-20 14:01:53 -0300

    adicionando interface UsuarioRepositorio

---
+++ src/main/java/com/upe/br/acheie/repositorio/UsuarioRepositorio.java
@@ -0,0 +1,13 @@
+package com.upe.br.acheie.repositorio;
+
+import com.upe.br.acheie.dominio.modelos.Usuario;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepositorio extends JpaRepository<Usuario, UUID> {
+
+  UserDetails findByEmail(String email);
+}
