a0e07f5852625e469e3cd49d2f484eb8cf5c8670
MateusVasc <109766889+MateusVasc@users.noreply.github.com>
2024-05-20 13:59:44 -0300

    adicionando dto CadastroRequest

---
+++ src/main/java/com/upe/br/acheie/dominio/dto/CadastroRequest.java
@@ -0,0 +1,11 @@
+package com.upe.br.acheie.dominio.dto;
+
+import com.upe.br.acheie.dominio.enums.Curso;
+import com.upe.br.acheie.dominio.enums.Periodo;
+import java.util.Date;
+
+public record CadastroRequest(String nome, String sobrenome, String email, String senha,
+                              Curso curso, Periodo periodo, String telefone,
+                              byte[] foto, Date criacaoDaConta) {
+
+}
