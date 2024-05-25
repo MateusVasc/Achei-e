4030f0c6b0da3c5e699e98f6e09210d5b89427fd
MateusVasc <109766889+MateusVasc@users.noreply.github.com>
2024-05-25 18:32:58 -0300

    refac: atualizando mappings dos controladores

--- src/main/java/com/upe/br/acheie/controlador/PostControle.java
+++ src/main/java/com/upe/br/acheie/controlador/PostControle.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,7 @@ import com.upe.br.acheie.servico.ComentarioServico;
 import com.upe.br.acheie.servico.PostServico;
 
 @RestController
+@RequestMapping("/achei-e")
 public class PostControle {
 
 	@Autowired
