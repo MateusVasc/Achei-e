bd432e421ba968f29f2374097472cb5b73de707a
MateusVasc <109766889+MateusVasc@users.noreply.github.com>
2024-06-12 14:07:40 -0300

    refac: Refatoração do serviço de comentários

--- src/main/java/com/upe/br/acheie/servico/ComentarioServico.java
+++ src/main/java/com/upe/br/acheie/servico/ComentarioServico.java
@@ -3,12 +3,8 @@ package com.upe.br.acheie.servico;
 import com.upe.br.acheie.dominio.dto.response.ExcluirComentarioResponse;
 import com.upe.br.acheie.repositorio.UsuarioRepositorio;
 import java.time.LocalDate;
-import java.util.Optional;
 import java.util.UUID;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.OptimisticLockingFailureException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import com.upe.br.acheie.dominio.dto.ComentarioDto;
@@ -22,61 +18,38 @@ import com.upe.br.acheie.dominio.utils.MensagensErro;
 import com.upe.br.acheie.dominio.utils.enums.Cadastro;
 
 @Service
+@RequiredArgsConstructor
 public class ComentarioServico {
 
-  @Autowired
-  private ComentarioRepositorio comentarioRepo;
+  private final ComentarioRepositorio comentarioRepo;
 
-  @Autowired
-  private PostRepositorio postRepo;
+  private final PostRepositorio postRepo;
 
-  @Autowired
-  private UsuarioRepositorio usuarioRepo;
-
-  private final Logger log = LogManager.getLogger(ComentarioServico.class);
+  private final UsuarioRepositorio usuarioRepo;
 
   public Cadastro cadastrarComentario(UUID postId, UUID usuarioId, ComentarioDto comentario) {
-    try {
-      Post post = postRepo.getReferenceById(
-          postId); //consertar para impedir que se post for nulo, consiga retornar successo
-      Usuario usuario = usuarioRepo.getReferenceById(
-          usuarioId); //consertar para impedir que se user for nulo retorne successo
-      comentarioRepo.save(new Comentario(comentario, post, usuario));
-      return Cadastro.SUCESSO_CADASTRO;
-    } catch (Exception e) {
-      this.tratarErros(e);
-    }
-    return Cadastro.ERRO_CADASTRO;
+    Post post = postRepo.findById(postId)
+        .orElseThrow(() -> new AcheieException(MensagensErro.MSG_POST_NAO_ENCONTRADO));
+    Usuario usuario = usuarioRepo.findById(usuarioId)
+        .orElseThrow(() -> new AcheieException(MensagensErro.MSG_USUARIO_NAO_ENCONTRADO));
+
+    comentarioRepo.save(new Comentario(comentario, post, usuario));
+
+    return Cadastro.SUCESSO_CADASTRO;
   }
 
   public ExcluirComentarioResponse excluirComentarioPorId(UUID idPost, UUID idUsuario,
       UUID idComentario) {
-    Optional<Comentario> comentarioParaRemover = this.comentarioRepo.findByUsuarioIdAndPostIdAndComentarioId(
-        idUsuario, idPost, idComentario);
-
-    if (comentarioParaRemover.isEmpty()) {
-      throw new AcheieException("Não existe um comentário para este id");
-    }
+    Comentario comentarioParaRemover = this.comentarioRepo.findByUsuarioIdAndPostIdAndComentarioId(
+        idUsuario, idPost, idComentario).orElseThrow(
+        () -> new AcheieException(MensagensErro.MSG_COMENTARIO_DE_USUARIO_NAO_ENCONTRADO));
 
     this.comentarioRepo.deleteById(idComentario);
-    comentarioParaRemover.get().setRemocaoDoComentario(LocalDate.now());
+    comentarioParaRemover.setRemocaoDoComentario(LocalDate.now());
 
-    return new ExcluirComentarioResponse(comentarioParaRemover.get().getCriacaoDoComentario(),
-        comentarioParaRemover.get().getRemocaoDoComentario(),
-        comentarioParaRemover.get().getUsuario().getId(),
-        comentarioParaRemover.get().getPost().getId());
-  }
-
-  public void tratarErros(Exception e) {
-    if (e instanceof IllegalArgumentException) {
-      log.error(MensagensErro.MSG_ELEMENTO_AUSENTE, e);
-      throw new AcheieException(MensagensErro.MSG_ELEMENTO_AUSENTE, e);
-    } else if (e instanceof OptimisticLockingFailureException) {
-      log.error(MensagensErro.MSG_ERRO_INESPERADO, e);
-      throw new AcheieException(MensagensErro.MSG_ERRO_INESPERADO, e);
-    } else {
-      log.error(e.getMessage(), e);
-      throw new AcheieException(e.getMessage(), e);
-    }
+    return new ExcluirComentarioResponse(comentarioParaRemover.getCriacaoDoComentario(),
+        comentarioParaRemover.getRemocaoDoComentario(),
+        comentarioParaRemover.getUsuario().getId(),
+        comentarioParaRemover.getPost().getId());
   }
 }
