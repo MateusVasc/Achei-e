0adaa27c210cab7b7f9252e39c39f21b2c7319f2
MateusVasc <109766889+MateusVasc@users.noreply.github.com>
2024-06-12 14:09:49 -0300

    refac: Refatoração do controlador de autenticação

--- src/main/java/com/upe/br/acheie/controlador/AutenticacaoControlador.java
+++ src/main/java/com/upe/br/acheie/controlador/AutenticacaoControlador.java
@@ -3,10 +3,8 @@ package com.upe.br.acheie.controlador;
 import com.upe.br.acheie.dominio.dto.request.CadastroRequest;
 import com.upe.br.acheie.dominio.dto.request.LoginRequest;
 import com.upe.br.acheie.servico.AutenticacaoServico;
-import com.upe.br.acheie.dominio.utils.MensagemUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,46 +18,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class AutenticacaoControlador {
 
   private final AutenticacaoServico autenticacaoServico;
-  private AuthenticationManager authenticationManager;
 
   @PostMapping("/login")
   public ResponseEntity<Object> login(@RequestBody LoginRequest request) {
-    ResponseEntity<Object> response;
-
-    try {
-      response = ResponseEntity.ok(this.autenticacaoServico.loginUsuario(request));
-    } catch (Exception e) {
-      response = ResponseEntity.badRequest()
-          .body(new MensagemUtil(e.getMessage()));
-    }
-
-    return response;
+    return ResponseEntity.ok(this.autenticacaoServico.loginUsuario(request));
   }
 
   @PostMapping("/cadastro")
   public ResponseEntity<Object> cadastro(@RequestBody CadastroRequest request) {
-    ResponseEntity<Object> response;
-
-    try {
-      response = ResponseEntity.ok(this.autenticacaoServico.cadastrarUsuario(request));
-    } catch (Exception e) {
-      response = ResponseEntity.badRequest()
-          .body(new MensagemUtil(e.getMessage()));
-    }
-
-    return response;
+    return ResponseEntity.ok(this.autenticacaoServico.cadastrarUsuario(request));
   }
 
   @DeleteMapping("/excluir/{email}")
   public ResponseEntity<Object> excluirPorEmail(@PathVariable("email") String email) {
-    ResponseEntity<Object> response;
-
-    try {
-      response = ResponseEntity.ok(this.autenticacaoServico.excluirUsuarioPorEmail(email));
-    } catch (Exception e) {
-      response = ResponseEntity.badRequest().body(new MensagemUtil(e.getMessage()));
-    }
-
-    return response;
+    return ResponseEntity.ok(this.autenticacaoServico.excluirUsuarioPorEmail(email));
   }
 }
