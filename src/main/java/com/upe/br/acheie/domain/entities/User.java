0e430015f05f0913d88360d6e0c67af4cdb7a266
MateusVasc <109766889+MateusVasc@users.noreply.github.com>
2024-09-18 19:31:05 -0300

    refac: Removes unused entity

--- src/main/java/com/upe/br/acheie/domain/entities/User.java
+++ src/main/java/com/upe/br/acheie/domain/entities/User.java
@@ -78,9 +78,6 @@ public class User implements UserDetails {
   @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
   private List<Comment> comments;
 
-  @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-  private List<Message> messages;
-
   public User(RegisterRequest request) {
     this.name = request.name();
     this.lastname = request.lastname();
