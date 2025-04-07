ac9f74a2b131a51f8ffedc809a5e422e42e911a1
MateusVasc <mvaraujo2004@gmail.com>
2025-04-06 22:11:56 -0300

    feat: adds custom exception messages

---
+++ auth-service/src/main/java/com/upe/br/auth/exception/ExceptionMessages.java
@@ -0,0 +1,10 @@
+package com.upe.br.auth.exception;
+
+public class ExceptionMessages {
+
+    public static final String USER_NOT_FOUND = "User not found";
+    public static final String FAILED_TO_CREATE_TOKEN = "Error while creating access token";
+    public static final String USER_ALREADY_EXISTIS = "Email already taken";
+
+    private ExceptionMessages() {}
+}
