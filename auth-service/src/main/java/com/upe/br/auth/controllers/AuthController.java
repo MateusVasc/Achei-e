44854cd960705179db90ad6cc82489502610e202
MateusVasc <mvaraujo2004@gmail.com>
2025-04-06 22:14:15 -0300

    feat: adds auth controller

---
+++ auth-service/src/main/java/com/upe/br/auth/controllers/AuthController.java
@@ -0,0 +1,37 @@
+package com.upe.br.auth.controllers;
+
+import com.upe.br.auth.dtos.LoginRequest;
+import com.upe.br.auth.dtos.LoginResponse;
+import com.upe.br.auth.dtos.RegisterRequest;
+import com.upe.br.auth.services.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Void> register(@Valid @RequestBody RegisterRequest request) {
+        this.authService.createUser(request);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest request) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(this.authService.login(request));
+    }
+}
