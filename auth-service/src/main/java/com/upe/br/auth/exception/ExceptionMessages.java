05e939d9a2448fedba7a655431ff35048e34304e
MateusVasc <mvaraujo2004@gmail.com>
2025-04-08 21:52:52 -0300

    chore: adds nes exception messages

--- auth-service/src/main/java/com/upe/br/auth/exception/ExceptionMessages.java
+++ auth-service/src/main/java/com/upe/br/auth/exception/ExceptionMessages.java
@@ -6,6 +6,9 @@ public class ExceptionMessages {
     public static final String FAILED_TO_CREATE_TOKEN = "Error while creating access token";
     public static final String USER_ALREADY_EXISTIS = "Email already taken";
     public static final String INVALID_CREDENTIALS = "Invalid credentials were provided";
+    public static final String INVALID_TOKEN = "An invalid token was provided";
+    public static final String TOKEN_WAS_REVOKED = "The refresh token was revoked";
+    public static final String FAILED_TO_GET_EXPIRATION_TIME = "Error while getting token expiration time";
 
     private ExceptionMessages() {}
 }
