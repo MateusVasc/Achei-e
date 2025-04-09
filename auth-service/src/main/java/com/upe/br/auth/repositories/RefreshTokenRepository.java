92b96314712cd8b7facc49737ac7a9b636186412
MateusVasc <mvaraujo2004@gmail.com>
2025-04-08 21:54:34 -0300

    feat: adds new methods to refresh token repository interface

--- auth-service/src/main/java/com/upe/br/auth/repositories/RefreshTokenRepository.java
+++ auth-service/src/main/java/com/upe/br/auth/repositories/RefreshTokenRepository.java
@@ -3,7 +3,12 @@ package com.upe.br.auth.repositories;
 import com.upe.br.auth.domain.entities.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
+
+    Optional<RefreshToken> findByToken(String token);
+
+    Optional<RefreshToken> findByTokenAndIsRevokedFalse(String token);
 }
