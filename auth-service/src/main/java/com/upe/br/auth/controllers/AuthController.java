695f63b4b3d25d6fa0a9e6a16729eb44b4444279
MateusVasc <mvaraujo2004@gmail.com>
2025-04-08 21:55:59 -0300

    feat: adds refresh token endpoint

--- auth-service/src/main/java/com/upe/br/auth/controllers/AuthController.java
+++ auth-service/src/main/java/com/upe/br/auth/controllers/AuthController.java
@@ -2,16 +2,14 @@ package com.upe.br.auth.controllers;
 
 import com.upe.br.auth.dtos.LoginRequest;
 import com.upe.br.auth.dtos.LoginResponse;
+import com.upe.br.auth.dtos.RefreshTokenRequest;
 import com.upe.br.auth.dtos.RegisterRequest;
 import com.upe.br.auth.services.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -33,5 +31,12 @@ public class AuthController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(this.authService.login(request));
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<LoginResponse> refreshToken(@Valid @RequestBody RefreshTokenRequest request) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(this.authService.refreshToken(request));
     }
 }
