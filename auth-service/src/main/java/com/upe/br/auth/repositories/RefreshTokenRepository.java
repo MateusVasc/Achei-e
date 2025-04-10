6319a3c98813c7e829ff56183b8fa82242fa18a0
MateusVasc <mvaraujo2004@gmail.com>
2025-04-10 10:15:36 -0300

    feat: adds new methods to RefreshTokenRepository

--- auth-service/src/main/java/com/upe/br/auth/repositories/RefreshTokenRepository.java
+++ auth-service/src/main/java/com/upe/br/auth/repositories/RefreshTokenRepository.java
@@ -1,8 +1,11 @@
 package com.upe.br.auth.repositories;
 
 import com.upe.br.auth.domain.entities.RefreshToken;
+import com.upe.br.auth.domain.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -11,4 +14,12 @@ public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID
     Optional<RefreshToken> findByToken(String token);
 
     Optional<RefreshToken> findByTokenAndIsRevokedFalse(String token);
+
+    long countByUserAndIsRevokedFalse(User user);
+
+    Optional<RefreshToken> findTopByUserAndIsRevokedFalseOrderByCreatedAtAsc(User user);
+
+    List<RefreshToken> findByUserAndExpiresAtBeforeAndIsRevokedFalse(User user, LocalDateTime dateTime);
+
+    List<RefreshToken> findByUserAndIsRevokedFalseOrderByCreatedAtDesc(User user);
 }
