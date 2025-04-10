1a7ecb689e3211bea3f4ec306c840abf9fce80d4
MateusVasc <mvaraujo2004@gmail.com>
2025-04-10 10:12:42 -0300

    refac: updates request validations

--- auth-service/src/main/java/com/upe/br/auth/dtos/RegisterRequest.java
+++ auth-service/src/main/java/com/upe/br/auth/dtos/RegisterRequest.java
@@ -2,36 +2,38 @@ package com.upe.br.auth.dtos;
 
 import com.upe.br.auth.domain.enums.Major;
 import com.upe.br.auth.domain.enums.Semester;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.*;
 
 public record RegisterRequest(
-        @NotBlank(message = "Nome é obrigatório")
+        @NotBlank(message = "Name is a required field")
+        @Size(min = 2, max = 50, message = "Name must  have 2 to 50 letters")
+        @Pattern(regexp = "^[a-zA-ZÀ-ÿ\\s]+$", message = "Name must contain only letters")
         String name,
 
-        @NotBlank(message = "Sobrenome é obrigatório")
+        @NotBlank(message = "Lastname is a required field")
+        @Size(min = 2, max = 50, message = "Lastname must  have 2 to 50 letters")
+        @Pattern(regexp = "^[a-zA-ZÀ-ÿ\\s]+$", message = "Lastname must contain only letters")
         String lastname,
 
-        @NotBlank(message = "Email é obrigatório")
-        @Email(message = "Email inválido")
+        @NotBlank(message = "Email is a required field")
+        @Email(message = "Invalid email")
         String email,
 
-        @NotBlank(message = "Senha é obrigatória")
-        @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$",
-                message = "Senha deve ter no mínimo 8 caracteres, incluindo letras e números")
+        @NotBlank(message = "Password is a required field")
+        @Size(min = 8, message = "Password must have at least 8 chars")
+        @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]+$",
+                message = "Password must include an uppercase letter, a lowercase letter, a number and a special char")
         String password,
 
-        @NotNull(message = "Curso é obrigatório")
+        @NotNull(message = "Major is a required field")
         Major major,
 
-        @NotNull(message = "Semestre é obrigatório")
+        @NotNull(message = "Semester is a required field")
         Semester semester,
 
-        @NotBlank(message = "Telefone é obrigatório")
+        @NotBlank(message = "Phone is a required field")
         @Pattern(regexp = "^\\+?[1-9][0-9]{10,14}$",
-                message = "Telefone inválido")
+                message = "Invalid phone number")
         String phone,
 
         byte[] image
