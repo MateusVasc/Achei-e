a5dd2a34db56013b04f52aa2f5b7dd4446c4e2f7
MateusVasc <mvaraujo2004@gmail.com>
2025-04-10 10:19:27 -0300

    feat: adds enablescheduling to main

--- auth-service/src/main/java/com/upe/br/auth/AuthApplication.java
+++ auth-service/src/main/java/com/upe/br/auth/AuthApplication.java
@@ -2,8 +2,10 @@ package com.upe.br.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class AuthApplication {
 
 	public static void main(String[] args) {
