53faac685d571cc82eb6054b8c5e28e8ce0bacef
MateusVasc <mvaraujo2004@gmail.com>
2025-04-10 10:12:55 -0300

    refac: updates request validations

--- auth-service/src/main/java/com/upe/br/auth/dtos/LoginRequest.java
+++ auth-service/src/main/java/com/upe/br/auth/dtos/LoginRequest.java
@@ -2,13 +2,18 @@ package com.upe.br.auth.dtos;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record LoginRequest(
-        @NotBlank(message = "Email é obrigatório")
-        @Email(message = "Email inválido")
+        @NotBlank(message = "Email is a required field")
+        @Email(message = "Invalid email")
         String email,
 
-        @NotBlank(message = "Senha é obrigatória")
+        @NotBlank(message = "Password is a required field")
+        @Size(min = 8, message = "Password must have at least 8 chars")
+        @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]+$",
+                message = "Password must include an uppercase letter, a lowercase letter, a number and a special char")
         String password
 ) {
 }
