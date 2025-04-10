e048a2b3bf9eca0b54d6ca65a328a973c47d910e
MateusVasc <mvaraujo2004@gmail.com>
2025-04-10 10:13:28 -0300

    feat: adds new exception messages

--- auth-service/src/main/java/com/upe/br/auth/exception/ExceptionMessages.java
+++ auth-service/src/main/java/com/upe/br/auth/exception/ExceptionMessages.java
@@ -9,6 +9,11 @@ public class ExceptionMessages {
     public static final String INVALID_TOKEN = "An invalid token was provided";
     public static final String TOKEN_WAS_REVOKED = "The refresh token was revoked";
     public static final String FAILED_TO_GET_EXPIRATION_TIME = "Error while getting token expiration time";
+    public static final String ACCOUNT_NOT_ENABLED = "Account is not enabled";
+    public static final String ACCOUNT_LOCKED = "Account was temporarily blocked";
+    public static final String TOO_MANY_ATTEMPTS = "Too many login attempts. Try again later";
+    public static final String TOKEN_EXPIRED = "Refresh token has expired";
+    public static final String TOKEN_NOT_BELONG_TO_USER = "Token does not belong to the user";
 
     private ExceptionMessages() {}
 }
