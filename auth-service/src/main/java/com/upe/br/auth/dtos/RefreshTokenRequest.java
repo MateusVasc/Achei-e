2966d9e9ac21382434d5b9a7a5238a7c0ab5e103
MateusVasc <mvaraujo2004@gmail.com>
2025-04-12 01:02:43 -0300

    refac: adds new field to RefreshTokenRequest

--- auth-service/src/main/java/com/upe/br/auth/dtos/RefreshTokenRequest.java
+++ auth-service/src/main/java/com/upe/br/auth/dtos/RefreshTokenRequest.java
@@ -3,6 +3,8 @@ package com.upe.br.auth.dtos;
 import jakarta.validation.constraints.NotBlank;
 
 public record RefreshTokenRequest(
+        String accessToken,
+
         @NotBlank
         String refreshToken
 ) {
