41479d258250e9df21a3dbc8ca25505156dd9860
MateusVasc <mvaraujo2004@gmail.com>
2025-04-13 00:41:21 -0300

    feat: adds eureka-server

---
+++ eureka/src/test/java/com/upe/br/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upe.br.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
